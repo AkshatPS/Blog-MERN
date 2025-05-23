3d45cbbb436170924aab2f18363fd696f642727f
Akshat Pratap SIngh <akshat.22bce7761@vitapstudent.ac.in>
2025-05-23 17:15:43 +0530

    Made changes in sidebar and posts components

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import "./posts.css";
 import Post from '../post/Post';
 
-export default function Posts( {posts}) {
-    return (
-        <div className="posts">
-            {posts.map((p) => (
-                <Post post={p} />
-            ))}
-        </div>  
-    )
+export default function Posts({ posts }) {
+  return (
+    <div className="posts">
+      {Array.isArray(posts) && posts.map((p) => (
+        <Post key={p._id || p.id} post={p} />
+      ))}
+    </div>  
+  );
 }
